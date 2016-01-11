6991e9c5007dca8de763850f2cc0404f7ac10bb2
ndixit918 <ndixit918@gmail.com>
2016-01-11 13:13:06 -0500

    Homework 6 First Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		@post = Post.create(post_params)
+		redirect_to post_path(@post)
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:content)
+	end
+end
